1f2c4b7f1ae572662d341c65d34dd2cb5e3321d7
Naimah Choudhury <naimah.choudhury98@gmail.com>
2026-02-23 16:19:04 +0000

    ingredientsnowdrags

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
-// import { useState } from 'react'
-import Stack from './components/Stack'
-import Ingredient from '.components/Ingredient'
-import { BrowserRouter } from 'react-router-dom'
-import './App.css'
+import { DndContext } from '@dnd-kit/core'
+import Ingredient from './components/Ingredient'
+
+const ingredients = [
+  { id: 1, name: "Bread", emoji: "🍞" },
+  { id: 2, name: "Lettuce", emoji: "🥬" },
+  { id: 3, name: "Cheese", emoji: "🧀" },
+  { id: 4, name: "Bacon", emoji: "🥓" },
+  { id: 5, name: "Tomato", emoji: "🍅" },
+  { id: 6, name: "Egg", emoji: "🍳" },
+  { id: 7, name: "Ham", emoji: "🍖" },
+  { id: 8, name: "Mayo", emoji: "🫙" },
+  { id: 9, name: "Cucumber", emoji: "🥒" },
+  { id: 10, name: "Avocado", emoji: "🥑" },
+]
 
 export default function App() {
-
   return (
+    <DndContext>
       <div>
-
+        <h1>Sandwich Maker 🥪</h1>
+        {ingredients.map(ingredient => (
+          <Ingredient
+            key={ingredient.id}
+            id={ingredient.id}
+            name={ingredient.name}
+            emoji={ingredient.emoji}
+          />
+        ))}
       </div>
-  )}
-
-  // stack in middle
+    </DndContext>
+  )
+}
